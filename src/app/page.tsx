c4000917079771e282715c6ce2719de03dbdeef0
Bitupan Moran <bitupanmoran007@gmail.com>
2026-03-20 03:00:04 +0000

    In the dashboard i.e the first page  , store this values matching the ae

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/components/Hero';
+import { DashboardSection } from '@/components/DashboardSection';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Hero />
+      <DashboardSection />
     </div>
   );
 }
